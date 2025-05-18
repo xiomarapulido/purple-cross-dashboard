51d73dd7da938f170238c153959cb362cc677a61
Xiomara <xiomaraa.pulido@gmail.com>
2025-05-18 05:26:43 -0700

    adding format dates

---
+++ src/constants/employeeTableConstants.ts
@@ -0,0 +1,33 @@
+export const TABLE_HEADERS = {
+    name: 'Name',
+    department: 'Department',
+    position: 'Position',
+    hired: 'Hired',
+    terminationDate: 'Termination Date',
+    actions: 'Actions',
+  }
+  
+  export const BUTTON_LABELS = {
+    exportCSV: 'Export to CSV',
+    importCSV: 'Import from CSV',
+    previous: 'Previous',
+    next: 'Next',
+  }
+  
+  export const PLACEHOLDERS = {
+    search: 'Search...',
+  }
+  
+  export const MESSAGES = {
+    noResults: 'No matching employees found.',
+    rowsPerPage: 'Rows per page:',
+  }
+  
+  export const CSV_HEADERS = [
+    TABLE_HEADERS.name,
+    TABLE_HEADERS.department,
+    TABLE_HEADERS.position,
+    TABLE_HEADERS.hired,
+    TABLE_HEADERS.terminationDate,
+  ]
+  
